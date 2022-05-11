7ced497f2d11e9ce1189cce1d3793e45f077aa21
-Unknown- <mrunknownyt@gmail.com>
2022-05-10 22:32:15 -0600

    Config

--- SlabGen/src/main/java/mrunknown404/slabgen/SlabGen.java
+++ SlabGen/src/main/java/mrunknown404/slabgen/SlabGen.java
@@ -2,13 +2,16 @@ package mrunknown404.slabgen;
 
 import mrunknown404.slabgen.utils.ClientProxy;
 import mrunknown404.slabgen.utils.SGRegistry;
+import mrunknown404.slabgen.utils.SlabGenConfig;
 import mrunknown404.slabgen.world.SGFeatures;
 import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(SlabGen.MOD_ID)
@@ -20,11 +23,16 @@ public class SlabGen {
 		bus.addListener(ClientProxy::clientSetup);
 		
 		MinecraftForge.EVENT_BUS.register(this);
+		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SlabGenConfig.COMMON_SPEC);
 		SGRegistry.register(bus);
 	}
 	
 	@SubscribeEvent
 	public void biomeLoad(BiomeLoadingEvent e) {
-		e.getGeneration().addFeature(Decoration.TOP_LAYER_MODIFICATION.ordinal(), () -> SGFeatures.GROUND_SLABS);
+		boolean isWhitelist = SlabGenConfig.COMMON.biomeWhiteList.get(), contains = SlabGenConfig.COMMON.biomeList.get().contains(e.getName().toString());
+		
+		if ((isWhitelist && contains) || (!isWhitelist && !contains)) {
+			e.getGeneration().addFeature(Decoration.TOP_LAYER_MODIFICATION.ordinal(), () -> SGFeatures.GROUND_SLABS);
+		}
 	}
 }
