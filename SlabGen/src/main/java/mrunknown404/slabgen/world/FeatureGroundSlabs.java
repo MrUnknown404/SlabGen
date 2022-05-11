79df0bb3ff4c4a38ee3aef8ea9583ad4040f07b4
-Unknown- <mrunknownyt@gmail.com>
2022-05-11 14:46:51 -0600

    Addon support

--- SlabGen/src/main/java/mrunknown404/slabgen/world/FeatureGroundSlabs.java
+++ SlabGen/src/main/java/mrunknown404/slabgen/world/FeatureGroundSlabs.java
@@ -27,8 +27,8 @@ public class FeatureGroundSlabs extends Feature<NoneFeatureConfiguration> {
 	private static final Set<Block> SIDE_BLOCKS = new HashSet<Block>();
 	
 	static {
-		SIDE_BLOCKS.add(Blocks.SANDSTONE);
-		SIDE_BLOCKS.add(Blocks.RED_SANDSTONE);
+		addSideBlock(Blocks.SANDSTONE);
+		addSideBlock(Blocks.RED_SANDSTONE);
 		add(Blocks.GRASS_BLOCK, Blocks.DIRT, SGBlocks.GRASS_SLAB.get());
 		add(Blocks.MYCELIUM, Blocks.DIRT, SGBlocks.MYCELIUM_SLAB.get());
 		add(Blocks.COARSE_DIRT, null, SGBlocks.COARSE_DIRT_SLAB.get());
@@ -103,9 +103,13 @@ public class FeatureGroundSlabs extends Feature<NoneFeatureConfiguration> {
 		return true;
 	}
 	
-	private static void add(Block originBlock, Block replaceBlock, Block slabBlock) {
+	public static void add(Block originBlock, Block replaceBlock, Block slabBlock) {
 		SPAWN_MAP.put(originBlock, new SlabSpawnInfo(replaceBlock, slabBlock));
-		SIDE_BLOCKS.add(originBlock);
+		addSideBlock(originBlock);
+	}
+	
+	public static void addSideBlock(Block block) {
+		SIDE_BLOCKS.add(block);
 	}
 	
 	private static boolean isSide(Block b) {
