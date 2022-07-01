e29b7c1995443e1a64b6aa76d8417af0a630fba4
-Unknown- <mrunknownyt@gmail.com>
2022-06-30 20:59:43 -0600

    Update SlabGenConfig.java

--- SlabGen/src/main/java/mrunknown404/slabgen/utils/SlabGenConfig.java
+++ SlabGen/src/main/java/mrunknown404/slabgen/utils/SlabGenConfig.java
@@ -14,7 +14,7 @@ public class SlabGenConfig {
 		public final ConfigValue<List<String>> biomeList;
 		
 		public Common(ForgeConfigSpec.Builder builder) {
-			builder.push("dice");
+			builder.push("slabgen");
 			this.biomeWhiteList = builder.comment("Wheather or not the biome list is whitelist or blacklist. false for blacklist, true for whitelist")
 					.define("biome whitelist", false);
 			this.biomeList = builder.comment("Biomes that should include/exclude slab generation. Should be <modid:biomeid>. Example: minecraft:forest").define("biome list",
